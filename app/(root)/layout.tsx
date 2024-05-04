3f351cafe309f3e6117cad37531090f089e52451
Phil <philwekullo@gmail.com>
2024-05-04 09:38:38 +0300

    feat: sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import  Sidebar  from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName: 'Phil', lastName: 'Wekullo'}
   return (
-    <main>
-        SIDEBAR
+    <main className="flex w-full h-screen font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
     </main>
   );
