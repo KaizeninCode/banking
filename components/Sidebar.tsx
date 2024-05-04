d0ea121dbdb94b64deb98e1fc1056f842166d097
Phil <philwekullo@gmail.com>
2024-05-04 12:39:08 +0300

    feat: fix sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,44 +8,46 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Sidebar = ({user}: SidebarProps) => {
-
     const pathname = usePathname();
-    <section className='sidebar'>
-        <nav className="flex flex-col gap-4">
-            <Link href='/' className='mb-12 cursor-pointer flex items-center gap-2'>
-                <Image 
-                    src='/icons/logo.svg'
-                    width={34}
-                    height={34}
-                    alt='Logo'
-                    className='size-[24px] max-xl:size-14'
-                />
-                <h1 className='sidebar-logo'>Horizon</h1>
-            </Link>
+    return (
 
-            {sidebarLinks.map(item => {
-                const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
-                return (
-                    <Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}>
-                        <div className='relative size-6'>
-                        <Image 
-                            src={item.imgURL}
-                            alt={item.label}
-                            fill
-                            className={cn({'brightness-[3] invert-0': isActive})}
-                         />
-                        </div>
-                        <p className={cn("sidebar-label", { "!text-white": isActive })}>
-                            {item.label}
-                        </p>
-                    </Link>
-                )
-            })}
+        <section className='sidebar'>
+            <nav className="flex flex-col gap-4">
+                <Link href='/' className='mb-12 cursor-pointer flex items-center gap-2'>
+                    <Image 
+                        src='/icons/logo.svg'
+                        width={34}
+                        height={34}
+                        alt='Logo'
+                        className='size-[24px] max-xl:size-14'
+                    />
+                    <h1 className='sidebar-logo'>Horizon</h1>
+                </Link>
 
-            USER
-        </nav>
-        FOOTER
-    </section>
+                {sidebarLinks.map(item => {
+                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+                    return (
+                        <Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}>
+                            <div className='relative size-6'>
+                            <Image 
+                                src={item.imgURL}
+                                alt={item.label}
+                                fill
+                                className={cn({'brightness-[3] invert-0': isActive})}
+                            />
+                            </div>
+                            <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                                {item.label}
+                            </p>
+                        </Link>
+                    )
+                })}
+
+                USER
+            </nav>
+            FOOTER
+        </section>
+    )
 }
 
 
